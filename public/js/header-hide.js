d1045ded69213fe9aab6c0f509a83efd5a226cbd
MaiLong <mai69338@gmail.com>
2023-02-08 15:18:23 +0700

    Bổ sung thanh tìm kiếm, layout Header lên lại bằng grid, thêm các biến màu cơ sở, hiệu ứng ẩn hiện menu...

---
+++ public/js/header-hide.js
@@ -0,0 +1,12 @@
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("js-header-hide").style.top = "0";
+  } else {
+    document.getElementById("js-header-hide").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+};
